43eaeaecb91a96a79ce0c80c02ba7d6f62054ca3
Thuan Ho <hodacthuan94@gmail.com>
2020-10-31 17:01:06 +0700

    Correct new models name

--- server/page_scrape/mrcong.py
+++ server/page_scrape/mrcong.py
@@ -1,44 +1,51 @@
 
-import csv, time,random,requests
+import csv
+import time
+import random
+import requests
 from bs4 import BeautifulSoup
-from page_scrape.models import Post
+from page_scrape.models import Album
 # https://www.digitalocean.com/community/tutorials/how-to-scrape-web-pages-with-beautiful-soup-and-python-3
 
 
 mainUrl = "https://mrcong.com/"
-source='mrcong'
+source = 'mrcong'
 
 
-def scrapeEachPost(url,thumbnail):
-    postFoundInDB = Post.objects(url=url,source=source)
-    if ~(len(postFoundInDB)==0): 
+def scrapeEachPost(url, thumbnail):
+    postFoundInDB = Album.objects(url=url, source=source)
+    if ~(len(postFoundInDB) == 0):
         time.sleep(5)
         html = BeautifulSoup(requests.get(url).text, 'html.parser')
-        title = html.find(class_='post-title').find('span').contents[0].split('(')[0]
-        
-        print('Scrape post:',title)
-        paginationHtml = html.find(class_='post-inner').find(class_='entry').find(class_='page-link').find_all('a')
-        paginationUrlList =[]
+        title = html.find(
+            class_='post-title').find('span').contents[0].split('(')[0]
+
+        print('Scrape post:', title)
+        paginationHtml = html.find(
+            class_='post-inner').find(class_='entry').find(class_='page-link').find_all('a')
+        paginationUrlList = []
         paginationUrlList.append(url)
         for pagination in paginationHtml:
-            paginationUrl =  pagination.get('href');
+            paginationUrl = pagination.get('href')
             paginationUrlList.append(paginationUrl)
         # print(paginationUrlList)
-        images=[]
+        images = []
         for paginationUrl in paginationUrlList:
-            print('Scrape page:',paginationUrl)
+            print('Scrape page:', paginationUrl)
             time.sleep(1)
-            html = BeautifulSoup(requests.get(paginationUrl).text, 'html.parser')
-            imagesHtmlList = html.find(class_='post-inner').find(class_='entry').find('p').find_all('img')
-            
+            html = BeautifulSoup(requests.get(
+                paginationUrl).text, 'html.parser')
+            imagesHtmlList = html.find(
+                class_='post-inner').find(class_='entry').find('p').find_all('img')
+
             for imageHtml in imagesHtmlList:
-                imagelink =  imageHtml.get('src');
-                images.append({"sourceUrl":imagelink,"publish":False})
-       
-        print('Total images',len(images))
-        post = Post(title=title, source=source, url=url,images=images,thumbnail=thumbnail )
+                imagelink = imageHtml.get('src')
+                images.append({"sourceUrl": imagelink, "publish": False})
+
+        print('Total images', len(images))
+        post = Album(title=title, source=source, url=url,
+                     images=images, thumbnail=thumbnail)
         post.save()
-    
 
 
 def scrapeMainPage():
@@ -46,17 +53,15 @@ def scrapeMainPage():
     # last_links.decompose()
     postHtmlList = html.find(class_='post-listing').find_all('article')
     for postHtml in postHtmlList:
-        postUrl =  postHtml.find(class_='post-thumbnail').find('a').get('href')
-        thumbnail = postHtml.find(class_='post-thumbnail').find('a').find('img').get('src')
-        if postUrl :
-            scrapeEachPost(postUrl,thumbnail)
+        postUrl = postHtml.find(class_='post-thumbnail').find('a').get('href')
+        thumbnail = postHtml.find(
+            class_='post-thumbnail').find('a').find('img').get('src')
+        if postUrl:
+            scrapeEachPost(postUrl, thumbnail)
 
 # scrapeMainPage()
-
-
 
 
 # for i in range(1, 5):
 #     url = 'https://web.archive.org/web/20121007172955/https://www.nga.gov/collection/anZ' + str(i) + '.htm'
 #     pages.append(url)
-
--- server/page_scrape/xsnvshen.py
+++ server/page_scrape/xsnvshen.py
@@ -1,43 +1,45 @@
 
-import csv, time,random,requests
+import csv
+import time
+import random
+import requests
 from bs4 import BeautifulSoup
-from page_scrape.models import Post
+from page_scrape.models import Album
 import requests
 
 originUrl = "https://www.xsnvshen.com/"
 mainUrl = "https://www.xsnvshen.com/album/"
-source='xsnvshen'
+source = 'xsnvshen'
 
 
-def scrapeEachPost(url,thumbnail):
-    postFoundInDB = Post.objects(url=url,source=source)
-    if ~(len(postFoundInDB)==0):
-        print('Scrape url:',url)
+def scrapeEachPost(url, thumbnail):
+    postFoundInDB = Album.objects(url=url, source=source)
+    if ~(len(postFoundInDB) == 0):
+        print('Scrape url:', url)
 
         time.sleep(2)
-        html = BeautifulSoup(requests.get(url, verify = False).text, 'html.parser')
+        html = BeautifulSoup(requests.get(
+            url, verify=False).text, 'html.parser')
         title = html.find("img", {"id": "bigImg"}).get('alt')
-        
-        
-        firstImageUrl = html.find("img", {"id": "bigImg"}).get('src').replace('//','https://')
+
+        firstImageUrl = html.find("img", {"id": "bigImg"}).get(
+            'src').replace('//', 'https://')
         imageUrl = firstImageUrl.split('/000.')
-        print('First image url:',imageUrl)
+        print('First image url:', imageUrl)
         bot = 0
         top = 100
-        while ((top-bot)>1):
+        while ((top-bot) > 1):
             num = int((top+bot)/2)
-            numStr =('0000' +str(num))[-3:]
-           
-            indexUrl= imageUrl[0]+'/'+numStr+'.'+imageUrl[1]
+            numStr = ('0000' + str(num))[-3:]
+
+            indexUrl = imageUrl[0]+'/'+numStr+'.'+imageUrl[1]
             print(indexUrl)
-            if (requests.get(indexUrl).status_code==200):
+            if (requests.get(indexUrl).status_code == 200):
                 bot = num
-                print('num',num)
+                print('num', num)
             else:
                 top = num
-        
-        
-        
+
         # print('bot',bot)
         # images=[]
         # for paginationUrl in paginationUrlList:
@@ -45,29 +47,31 @@ def scrapeEachPost(url,thumbnail):
         #     time.sleep(1)
         #     html = BeautifulSoup(requests.get(paginationUrl).text, 'html.parser')
         #     imagesHtmlList = html.find(class_='post-inner').find(class_='entry').find('p').find_all('img')
-            
+
         #     for imageHtml in imagesHtmlList:
         #         imagelink =  imageHtml.get('src');
         #         images.append({"sourceUrl":imagelink,"publish":False})
-       
+
         # print('Total images',len(images))
-        # post = Post(title=title, source=source, url=url,images=images,thumbnail=thumbnail )
+        # post = Album(title=title, source=source, url=url,images=images,thumbnail=thumbnail )
         # post.save()
-    
 
 
 def scrapeMainPage():
-    html = BeautifulSoup(requests.get(mainUrl, verify = False ).text, 'html.parser')
-    
-    postHtmlList = html.find(class_='index_listc').find(class_='pos_6_1').find('ul').find_all('li')
-  
-    for postHtml in postHtmlList:
-        postUrl =mainUrl+ postHtml.find('a').get('href').split('/')[2]
+    html = BeautifulSoup(requests.get(
+        mainUrl, verify=False).text, 'html.parser')
 
-        thumbnail = postHtml.find('a').find('img').get('src').replace('//','https://')
-     
+    postHtmlList = html.find(class_='index_listc').find(
+        class_='pos_6_1').find('ul').find_all('li')
+
+    for postHtml in postHtmlList:
+        postUrl = mainUrl + postHtml.find('a').get('href').split('/')[2]
+
+        thumbnail = postHtml.find('a').find(
+            'img').get('src').replace('//', 'https://')
+
         if postUrl:
-            scrapeEachPost(postUrl,thumbnail)
+            scrapeEachPost(postUrl, thumbnail)
+
 
 scrapeMainPage()
-
