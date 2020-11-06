ac43952ece89cbf56ff0a4442e7475142f8c4942
Thuan Ho <hodacthuan94@gmail.com>
2020-11-06 23:55:32 +0700

    Add new function to send telegram message

--- server/commons/telegram.py
+++ server/commons/telegram.py
@@ -1,8 +1,8 @@
 import telegram
 import os
+from sexybaby import constants
 
-TELEGRAM_TOKEN = os.environ['TELEGRAM_TOKEN']
-TELEGRAM_CHAT_ID = os.environ['TELEGRAM_CHAT_ID']
 
-autoBot = telegram.Bot(token=TELEGRAM_TOKEN)
-# autoBot.send_message(chat_id=TELEGRAM_CHAT_ID, text='TEST')
+def sendMessage(message):
+    autoBot = telegram.Bot(token=constants.TELEGRAM_TOKEN)
+    autoBot.send_message(chat_id=constants.TELEGRAM_CHAT_ID, text=message)
